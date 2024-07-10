921603119ce488a5f1b111d6c17903b061c0d3e5
Lama Alosaimi <lama@Lamas-MacBook-Pro.local>
2024-07-10 11:29:43 +0300

    updated code to implement goroutine

--- main.go
+++ main.go
@@ -12,38 +12,34 @@ import (
 	"github.com/beevik/etree"
 )
 
-func gitClone(repoURL, destination string) error {
+// Git clone function
+func gitClone(repoURL, destination string, done chan<- error) {
 	cmd := exec.Command("git", "clone", repoURL, destination)
-
-	// Set output to os.Stdout or os.Stderr if you want to see git clone command output
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	err := cmd.Run()
-	if err != nil {
-		return err
-	}
-
-	return nil
+	done <- err
 }
-func getPackageType(TMP_PATH string) string {
 
+// Get package type from pom.xml
+func getPackageType(TMP_PATH string, done chan<- string) {
 	doc := etree.NewDocument()
 	if err := doc.ReadFromFile(TMP_PATH + "/pom.xml"); err != nil {
 		fmt.Println("Error reading pom.xml:", err)
 		log.Fatal()
 	}
 
-	// Find the packaging element in the XML
 	packaging := doc.FindElement("//project/packaging")
 	if packaging == nil {
 		fmt.Println("Packaging element not found in pom.xml")
 		log.Fatal()
 	}
 
-	// Get the text value of the packaging element
-	return packaging.Text()
+	done <- packaging.Text()
 }
+
+// Delete sources JAR files from the target directory
 func deleteSourcesJar(targetDir string) error {
 	matches, err := filepath.Glob(filepath.Join(targetDir, "*-sources.*"))
 	if err != nil {
@@ -59,91 +55,101 @@ func deleteSourcesJar(targetDir string) error {
 
 	return nil
 }
-func buildExtension(TMP_PATH string) {
-	// Run Maven build
+
+// Build extension
+func buildExtension(TMP_PATH string, done chan<- error) {
 	cmd := exec.Command("mvn", "clean", "package", "-Dmaven.test.skip=true", "-Dclassifier=")
 	cmd.Dir = TMP_PATH
-
-	// Redirect stdout and stderr
-	// cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	fmt.Println("[INFO] Running Maven command: mvn clean package")
+
 	err := cmd.Run()
 	if err != nil {
-		log.Fatalf("[ERROR] running Maven command: %v", err)
+		done <- fmt.Errorf("[ERROR] running Maven command: %v", err)
+		return
 	}
 	fmt.Println("[INFO] Maven build completed successfully")
 
-	// Delete *-sources.* file from target directory
 	targetDir := filepath.Join(TMP_PATH, "target")
 	err = deleteSourcesJar(targetDir)
 	if err != nil {
-		log.Fatalf("[ERROR] deleting sources JAR file: %v", err)
+		done <- fmt.Errorf("[ERROR] deleting sources JAR file: %v", err)
+		return
 	}
 	fmt.Println("[INFO] Deleted *-sources.* file from target directory")
+
+	done <- nil
 }
-func copyBuildToProviders(extensionFile string) {
+
+// Copy the built file to the Keycloak providers directory
+func copyBuildToProviders(extensionFile string, done chan<- error) {
 	KEYCLOAK_PATH := os.Getenv("KEYCLOAK_PATH")
 	KEYCLOAK_PROVIDERS_PATH := filepath.Join(KEYCLOAK_PATH, "providers")
 	sourceFile, err := os.Open(extensionFile)
 	if err != nil {
-		log.Fatal(err.Error())
+		done <- err
+		return
 	}
 	defer sourceFile.Close()
 
-	// Create destination file
 	destinationFile, err := os.Create(filepath.Join(KEYCLOAK_PROVIDERS_PATH, extensionFile))
 	if err != nil {
-		log.Fatal(err.Error())
+		done <- err
+		return
 	}
 	defer destinationFile.Close()
 
-	// Copy the contents from source to destination
 	_, err = io.Copy(destinationFile, sourceFile)
 	if err != nil {
-		log.Fatal(err.Error())
+		done <- err
+		return
 	}
 
-	// Flushes memory to disk ensuring file copy
 	err = destinationFile.Sync()
 	if err != nil {
-		log.Fatal(err.Error())
+		done <- err
+		return
 	}
 
 	fmt.Println("File copied successfully.")
+	done <- nil
 }
-func rebuildKeycloakInstance() {
+
+// Rebuild Keycloak instance
+func rebuildKeycloakInstance(done chan<- error) {
 	fmt.Println("[INFO] Rebuild Keycloak ...")
 	KEYCLOAK_PATH := os.Getenv("KEYCLOAK_PATH")
 	cmd := exec.Command("bin/kc.sh", "build")
 	cmd.Dir = KEYCLOAK_PATH
-	// Run the command and capture the output
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Fatalf("[ERROR] running command: %v\n%s", err, output)
+		done <- fmt.Errorf("[ERROR] running command: %v\n%s", err, output)
+		return
 	}
 
-	// Print the output if needed
 	fmt.Println(string(output))
+	done <- nil
 }
-func restartKeycloak() {
+
+// Restart Keycloak service
+func restartKeycloak(done chan<- error) {
 	fmt.Println("[INFO] Restarting Keycloak Service")
 	KEYCLOAK_PATH := os.Getenv("KEYCLOAK_PATH")
 	cmd := exec.Command("systemctl", "restart", "keycloak")
 	cmd.Dir = KEYCLOAK_PATH
-	// Run the command and capture the output
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Fatalf("[ERROR] running command: %v\n%s", err, output)
+		done <- fmt.Errorf("[ERROR] running command: %v\n%s", err, output)
+		return
 	}
 
-	// Print the output if needed
 	fmt.Println(string(output))
+	done <- nil
 }
+
 func main() {
 
-	TMP_PATH := "/tmp/code"
-	// Define flags
+	TMP_PATH := "/Users/lama/development/go-projects/keycloak-clitool/tmp/code"
 	url := flag.String("url", "", "Github url of the extension")
 	flag.Parse()
 
@@ -153,34 +159,77 @@ func main() {
 	}
 
 	fmt.Printf("[INFO] Start downloading keycloak's extension [%s] ...\n", *url)
-	os.Mkdir(TMP_PATH, 0755)
-	// run download command in a goroutine
-	err := gitClone(*url, TMP_PATH)
-	if err != nil {
+
+	// Create a channel for error handling
+	done := make(chan error)
+
+	// Clone the Git repository
+	go gitClone(*url, TMP_PATH, done)
+
+	// Wait for cloning to finish
+	if err := <-done; err != nil {
 		fmt.Println("[ERROR] cloning repository:", err)
 		return
 	}
-	defer fmt.Printf("[INFO] Installed extension [%s] into keycloak's custom providers.\n", *url)
 
-	buildExtension(TMP_PATH)
-	// parse pom.xml to distinguish the packging type (war/jar)
-	packaging := getPackageType(TMP_PATH)
+	fmt.Println("[INFO] Cloning completed")
+
+	// Build the extension
+	go buildExtension(TMP_PATH, done)
+
+	// Wait for build to complete
+	if err := <-done; err != nil {
+		fmt.Println("[ERROR] building extension:", err)
+		return
+	}
+
+	fmt.Println("[INFO] Extension built successfully")
+
+	// Create a channel to receive the packaging type
+	packagingDone := make(chan string)
+	go getPackageType(TMP_PATH, packagingDone)
+	packaging := <-packagingDone
 	pattern := "*." + packaging
-	err = os.Chdir(filepath.Join(TMP_PATH, "target"))
+
+	err := os.Chdir(filepath.Join(TMP_PATH, "target"))
 	if err != nil {
 		log.Fatalf("Error changing directory to %s: %v", TMP_PATH, err)
 	}
+
 	matches, err := filepath.Glob(pattern)
 	if err != nil {
-		log.Fatalf("Error finding JAR file: %v", err)
+		log.Fatalf("Error finding build file: %v", err)
 	}
-	// Ensure exactly one JAR file is found
 	if len(matches) != 1 {
-		log.Fatalf("Expected exactly one JAR file, found %d", len(matches))
+		log.Fatalf("Expected exactly one build file, found %d", len(matches))
 	}
-	// Extract the file name from the path
+
 	buildFile := matches[0]
-	copyBuildToProviders(buildFile)
-	rebuildKeycloakInstance()
-	restartKeycloak()
+
+	// Copy the built file to the Keycloak providers directory
+	go copyBuildToProviders(buildFile, done)
+	if err := <-done; err != nil {
+		fmt.Println("[ERROR] copying build file:", err)
+		return
+	}
+
+	fmt.Println("[INFO] File copied successfully")
+
+	// Rebuild Keycloak instance
+	// go rebuildKeycloakInstance(done)
+	// if err := <-done; err != nil {
+	// 	fmt.Println("[ERROR] rebuilding Keycloak instance:", err)
+	// 	return
+	// }
+
+	fmt.Println("[INFO] Keycloak instance rebuilt")
+
+	// Restart Keycloak service
+	// go restartKeycloak(done)
+	// if err := <-done; err != nil {
+	// 	fmt.Println("[ERROR] restarting Keycloak service:", err)
+	// 	return
+	// }
+
+	fmt.Println("[INFO] Keycloak service restarted")
 }
